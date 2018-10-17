3b985cca0396da0f301e1b0f203b4fb1c9898550
Iain Rex <iain.rex@ROPTO-IMAC002.local>
2018-10-17 15:11:42 +0100

    Second commit

--- app/js/lazy-load.js
+++ app/js/lazy-load.js
@@ -5,43 +5,43 @@
 // Last updated July 2, 2018.
 //
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy-img"));
-    let active = false;
-
-    const lazyLoad = function() {
-        if (active === false) {
-        active = true;
-
-        setTimeout(function() {
-            lazyImages.forEach(function(lazyImage) {
-                if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-                    lazyImage.src = lazyImage.dataset.src;
-                    lazyImage.srcset = lazyImage.dataset.srcset;
-                    lazyImage.classList.remove("lazy");
-
-                    lazyImages = lazyImages.filter(function(image) {
-                    return image !== lazyImage;
-                });
-
-                if (lazyImages.length === 0) {
-                    document.removeEventListener("scroll", lazyLoad);
-                    window.removeEventListener("resize", lazyLoad);
-                    window.removeEventListener("orientationchange", lazyLoad);
-                }
-            }
-        });
-
-        active = false;
-        }, 200);
-        }
-    };
-
-    document.addEventListener("scroll", lazyLoad);
-    window.addEventListener("resize", lazyLoad);
-    window.addEventListener("orientationchange", lazyLoad);
-});
+// THIS PAGE ONLY NEEDS TO LAZY LOAD BACKGROUND IMAGES RIGHT NOW
+// document.addEventListener("DOMContentLoaded", function() {
+//     let lazyImages = [].slice.call(document.querySelectorAll("img.lazy-img"));
+//     let active = false;
+//
+//     const lazyLoad = function() {
+//         if (active === false) {
+//         active = true;
+//
+//         setTimeout(function() {
+//             lazyImages.forEach(function(lazyImage) {
+//                 if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+//                     lazyImage.src = lazyImage.dataset.src;
+//                     lazyImage.srcset = lazyImage.dataset.srcset;
+//                     lazyImage.classList.remove("lazy");
+//
+//                     lazyImages = lazyImages.filter(function(image) {
+//                     return image !== lazyImage;
+//                 });
+//
+//                 if (lazyImages.length === 0) {
+//                     document.removeEventListener("scroll", lazyLoad);
+//                     window.removeEventListener("resize", lazyLoad);
+//                     window.removeEventListener("orientationchange", lazyLoad);
+//                 }
+//             }
+//         });
+//
+//         active = false;
+//         }, 200);
+//         }
+//     };
+//
+//     document.addEventListener("scroll", lazyLoad);
+//     window.addEventListener("resize", lazyLoad);
+//     window.addEventListener("orientationchange", lazyLoad);
+// });
 
 
 /* Lazy load background images */
